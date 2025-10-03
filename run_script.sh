6a303326126827f52505e27ed4076a4d8e1458b9
Anton21a <shpilman.1995@gmail.com>
2025-10-03 18:58:18 +0300

    one-click script

--- run_script.sh
+++ run_script.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-set -e 
+set -e
+
 conda env create -f project_environment.yml || conda env update -f project_environment.yml
-
 conda run -n data-analysis python -m ipykernel install --user --name=data-analysis --display-name "Python (data-analysis)"
-
-conda run -n data-analysis python scripts/make_clean_subset.py
-
-conda run -n data-analysis papermill "Data-and-Analysis/Analysis.ipynb" "Data-and-Analysis/Analysis_out.ipynb"
+conda run -n data-analysis jupyter nbconvert --to notebook --execute Data-and-Analysis/Preprocessing_cleaning.ipynb --inplace
+conda run -n data-analysis jupyter nbconvert --to notebook --execute Data-and-Analysis/OLS_Analysis.ipynb --inplace
 
 
